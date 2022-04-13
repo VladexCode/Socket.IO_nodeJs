529444dd8fb18ee6a4f60e782c289b6886159641
VladexCode <vladfernd@gmail.com>
2022-04-12 23:15:36 -0400

    socketIO ticketera

--- models/server.js
+++ models/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const { socketController } = require('../socket/controller');
+const { socketController } = require('../sockets/controller');
 
 class Server {
 
